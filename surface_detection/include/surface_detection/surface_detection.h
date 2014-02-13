8d22d415fef13cedfed1b93bbbf45996331e13db
ros-industrial <ros-industrial@rosindustrial-ThinkPad-T61p.(none)>
2014-02-12 18:19:33 -0600

    added several point cloud processing methods and members

--- surface_detection/include/surface_detection/surface_detection.h
+++ surface_detection/include/surface_detection/surface_detection.h
@@ -16,13 +16,35 @@
 #include <pcl/kdtree/kdtree_flann.h>
 #include <pcl/features/normal_3d.h>
 #include <pcl/surface/gp3.h>
+#include <visualization_msgs/MarkerArray.h>
 
 namespace surface_detection {
 
 typedef pcl::PointCloud<pcl::PointXYZ> Cloud;
-typedef pcl::PointCloud<pcl::PointXYZ>::Ptr CloudPtr;
+typedef pcl::PointCloud<pcl::PointXYZRGB> CloudRGB;
+typedef pcl::PointCloud<pcl::Normal> Normals;
 
 static const std::string POINT_COLUD_TOPIC="sensor_point_cloud";
+static const float DEFAULT_ACQUISITION_TIME = 5.0f;
+
+static const int DEFAULT_STATISTICAL_OUTLIER_MEAN = 50;
+static const float DEFAULT_STATISTICAL_OUTLIER_STDEV_THRESHOLD = 1;
+static const int DEFAULT_K_SEARCH = 50;
+
+static const int DEFAULT_REGION_GROWING_MIN_CLUSTER_SIZE=100;
+static const int DEFAULT_REGION_GROWING_MAX_CLUSTER_SIZE=100000;
+static const int DEFAULT_REGION_GROWING_NEIGHBORS=50;
+static const float DEFAULT_REGION_GROWING_SMOOTHNESS_THRESHOLD=(7.0f )/( 180.0f * M_PI);
+static const float DEFAULT_REGION_GROWING_CURVATURE_THRESHOLD=1.0f;
+
+static const float DEFAULT_TRIANGULATION_SEARCH_RADIUS = 0.01;
+static const float DEFAULT_TRIANGULATION_MU = 2.5f;
+static const int DEFAULT_TRIANGULATION_MAX_NEAREST_NEIGHBORS = 100;
+static const float DEFAULT_TRIANGULATION_MAX_SURFACE_ANGLE = M_PI/4.0f;
+static const float DEFAULT_TRIANGULATION_MIN_ANGLE= M_PI/18.0f;
+static const float DEFAULT_TRIANGULATION_MAX_ANGLE = 2.0f *M_PI/3.0f;
+static const bool DEFAULT_TRIANGULATION_NORMAL_CONSISTENCY = false;
+
 class SurfaceDetection {
 
 
@@ -32,8 +54,14 @@ public:
 
 public:
 
-	void acquire_data(float duration);
-	bool find_surfaces();
+	void set_acquisition_time(float val);
+	void acquire_data();
+	bool find_surfaces(const Cloud& in);
+
+	// retrieve results
+	Cloud::Ptr get_acquired_cloud();
+	CloudRGB::Ptr get_region_colored_cloud();
+	std::vector<Cloud::Ptr> get_segment_clouds();
 
 protected:
 
@@ -41,16 +69,50 @@ protected:
 	void point_cloud_subscriber_cb(const sensor_msgs::PointCloud2ConstPtr &msg);
 	bool apply_statistical_filter(const Cloud& in,Cloud& out);
 	bool apply_region_growing_segmentation(const Cloud& in,
-			std::vector<Cloud>& segments);
-	bool apply_fast_triangulation(const std::vector<Cloud>& segments,
-			std::vector<pcl::PolygonMesh>& meshes);
+			const Normals& normals,
+			std::vector<pcl::PointIndices>& clusters,CloudRGB& colored_cloud);
+	bool apply_normal_estimation(const Cloud &cloud,Normals& normals);
+	bool apply_fast_triangulation(const Cloud& in,
+			const Normals& normals,
+			pcl::PolygonMesh& mesh);
 
 
 protected:
 
+	// roscpp members
 	ros::Subscriber point_cloud_subs_;
-	pcl::PointCloud<pcl::PointXYZ>::Ptr aggregate_cloud_ptr_;
+
+	// pcl members
+	pcl::PointCloud<pcl::PointXYZ>::Ptr acquired_cloud_ptr_;
+	CloudRGB::Ptr region_colored_cloud_ptr_;
+	std::vector<Cloud::Ptr> segment_clouds_;
+	visualization_msgs::MarkerArray meshes;
+
+	// acquisition
+	float acquisition_time_;
+
+	// filter and normal estimation
 	int acquired_clouds_counter_;
+	int meanK_;
+	int k_search_;
+	float stdv_threshold_;
+
+	// region growing
+	int rg_min_cluster_size_;
+	int rg_max_cluster_size_;
+	int rg_neightbors_;
+	float rg_smoothness_threshold_;
+	float rg_curvature_threshold_;
+
+	// fast triangulation
+	float tr_search_radius_;
+	float tr_mu_;
+	float tr_max_nearest_neighbors_;
+	float tr_max_surface_angle_;
+	float tr_min_angle_;
+	float tr_max_angle_;
+	bool tr_normal_consistency_;
+
 };
 
 } /* namespace surface_detection */
