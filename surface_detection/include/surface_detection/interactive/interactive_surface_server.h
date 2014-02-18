96d481a31f05e0969e73ee796b2686e983d65f06
ros-industrial <ros-industrial@rosindustrial-ThinkPad-T61p.(none)>
2014-02-18 15:26:25 -0600

    added arrow markers to show selection status of a surface

--- surface_detection/include/surface_detection/interactive/interactive_surface_server.h
+++ surface_detection/include/surface_detection/interactive/interactive_surface_server.h
@@ -21,7 +21,7 @@ namespace surface_detection {
 namespace interactive {
 
 namespace params{
-	static const std::string FRAME_ID = "frame_id";
+	static const std::string PARAMETER_NS = "interactive_surface_server";
 }
 
 namespace defaults{
@@ -30,14 +30,19 @@ namespace defaults{
 	static const std::string MARKER_DESCRIPTION = "surface";
 	static const std::string INTERACTIVE_MARKER_NAME = "select surface";
 	static const int MAX_TRIANGLES = 20;
-	static const double ARROW_LENGTH = 0.04f;
-	static const double ARROW_SHAFT_DIAMETER = 0.01f;
-	static const double ARROW_HEAD_DIAMETER = 0.02f;
-	static const double ARROW_HEAD_LENGTH = 0.01;
+	static const double ARROW_LENGTH = 0.08f;
+	static const double ARROW_SHAFT_DIAMETER = 0.02f;
+	static const double ARROW_HEAD_DIAMETER = 0.04f;
+	static const double ARROW_HEAD_LENGTH = 0.02f;
+	static const double ARROW_DISTANCE = 0.02f;
 }
 
 
 class InteractiveSurfaceServer {
+
+public:
+
+	typedef std::pair<std::string, bool> Selection;
 public:
 	InteractiveSurfaceServer();
 	virtual ~InteractiveSurfaceServer();
@@ -46,15 +51,15 @@ public:
 	bool init();
 	void run();
 
-	void add_marker(const visualization_msgs::Marker& marker);
-	void add_marker(const visualization_msgs::Marker& marker,const geometry_msgs::Pose& pose);
+	void add_surface(const visualization_msgs::Marker& marker);
+	void add_surface(const visualization_msgs::Marker& marker,const geometry_msgs::Pose& pose);
 	void add_random_surface_marker();
 
 protected:
 
 	interactive_markers::InteractiveMarkerServerPtr marker_server_ptr_;
 	interactive_markers::MenuHandler menu_handler_;
-	std::vector<std::string> marker_names_;
+	std::map<std::string,bool> surface_selection_map_;
 
 protected:
 
@@ -64,6 +69,8 @@ protected:
 			const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback);
 	void menu_marker_callback(
 			const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback);
+	void set_selection_flag(std::string marker_name,bool selected);
+	void toggle_selection_flag(std::string marker_name);
 	void create_polygon_marker(visualization_msgs::Marker& marker,int triangles);
 	void create_arrow_marker(const visualization_msgs::Marker& surface_marker,
 			visualization_msgs::Marker& arrow_marker);
@@ -71,14 +78,23 @@ protected:
 
 protected:
 
-	std::string frame_id_;
 	std::string marker_name_;
 	std::string marker_description_;
 
-	double arrow_lenght_;
+	double arrow_length_;
 	double arrow_shaft_diameter_;
 	double arrow_head_diameter_;
 	double arrow_head_length_;
+	double arrow_distance_;
+
+	// menu handling
+	uint32_t select_entry_id_;
+	uint32_t unselect_entry_id_;
+	uint32_t select_all_entry_id_;
+	uint32_t clear_all_entry_id_;
+	uint32_t hide_entry_id_;
+	uint32_t show_all_entry_id_;
+
 
 
 	// callbacks
--- surface_detection/src/interactive/interactive_surface_server.cpp
+++ surface_detection/src/interactive/interactive_surface_server.cpp
@@ -9,14 +9,21 @@
 #include <tf/transform_datatypes.h>
 #include <tf/tf.h>
 #include <sstream>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/common/common.h>
 
 namespace surface_detection {
 namespace interactive {
 
 InteractiveSurfaceServer::InteractiveSurfaceServer() :
-		frame_id_(defaults::FRAME_ID),
 		marker_name_(defaults::MARKER_SERVER_NAME),
-		marker_description_(defaults::MARKER_DESCRIPTION)
+		marker_description_(defaults::MARKER_DESCRIPTION),
+		arrow_distance_(defaults::ARROW_DISTANCE),
+		arrow_head_diameter_(defaults::ARROW_HEAD_DIAMETER),
+		arrow_head_length_(defaults::ARROW_HEAD_LENGTH),
+		arrow_length_(defaults::ARROW_LENGTH),
+		arrow_shaft_diameter_(defaults::ARROW_SHAFT_DIAMETER)
 {
 	// TODO Auto-generated constructor stub
 
@@ -47,11 +54,13 @@ void InteractiveSurfaceServer::run()
 				boost::bind(&InteractiveSurfaceServer::menu_marker_callback,this,_1));
 
 	// setup menu handler
-	menu_handler_.insert("Option 1",menu_callback_);
-	menu_handler_.insert("Option 2",menu_callback_);
-	interactive_markers::MenuHandler::EntryHandle submenu_handle = menu_handler_.insert("Submenu");
-	menu_handler_.insert(submenu_handle,"Submenu Option 1",menu_callback_);
-	menu_handler_.insert(submenu_handle,"Submenu Option 2",menu_callback_);
+	select_entry_id_ =  menu_handler_.insert("Select",menu_callback_);
+	unselect_entry_id_ =  menu_handler_.insert("Unselect",menu_callback_);
+	interactive_markers::MenuHandler::EntryHandle submenu_handle = menu_handler_.insert("More Options");
+	select_all_entry_id_ = menu_handler_.insert(submenu_handle,"Select All",menu_callback_);
+	clear_all_entry_id_ =  menu_handler_.insert(submenu_handle,"Clear Selections",menu_callback_);
+	hide_entry_id_ = menu_handler_.insert(submenu_handle,"Hide",menu_callback_);
+	show_all_entry_id_ = menu_handler_.insert(submenu_handle,"Show All",menu_callback_);
 
 	marker_server_ptr_->applyChanges();
 }
@@ -59,16 +68,36 @@ void InteractiveSurfaceServer::run()
 bool InteractiveSurfaceServer::load_parameters()
 {
 	ros::NodeHandle nh("~");
-	bool succeeded = nh.getParam(params::FRAME_ID,frame_id_);
-	if(succeeded)
-	{
-		ROS_INFO_STREAM("surface server finished loading parameters");
-	}
-	else
-	{
-		ROS_ERROR_STREAM("surface server failed to load parameters");
-	}
+	std::string ns = params::PARAMETER_NS + "/";
+	bool succeeded = true;//nh.getParam(ns +params::FRAME_ID,frame_id_);
 	return succeeded;
+}
+
+void InteractiveSurfaceServer::set_selection_flag(std::string marker_name,bool selected)
+{
+
+	visualization_msgs::InteractiveMarker int_marker;
+	if(surface_selection_map_.count(marker_name)>0 && marker_server_ptr_->get(marker_name,int_marker))
+	{
+		surface_selection_map_[marker_name] = selected;
+		int_marker.controls[1].markers[0].color.a = selected ? 1 : 0;
+		//int_marker.controls[1].always_visible = selected;
+		marker_server_ptr_->insert(int_marker);
+	}
+}
+
+void InteractiveSurfaceServer::toggle_selection_flag(std::string marker_name)
+{
+	visualization_msgs::InteractiveMarker int_marker;
+	bool selected;
+	if(surface_selection_map_.count(marker_name)>0 && marker_server_ptr_->get(marker_name,int_marker))
+	{
+		selected = surface_selection_map_[marker_name];
+		int_marker.controls[1].markers[0].color.a = !selected ? 1 : 0;
+		surface_selection_map_[marker_name] = !selected;
+		//int_marker.controls[1].always_visible = selected;
+		marker_server_ptr_->insert(int_marker);
+	}
 }
 
 void InteractiveSurfaceServer::button_marker_callback(
@@ -78,12 +107,11 @@ void InteractiveSurfaceServer::button_marker_callback(
 	{
 	case visualization_msgs::InteractiveMarkerFeedback::BUTTON_CLICK:
 		ROS_INFO_STREAM("marker "<<feedback->marker_name <<" button control was clicked");
+		toggle_selection_flag(feedback->marker_name);
 		marker_server_ptr_->applyChanges();
 		break;
 
 	case visualization_msgs::InteractiveMarkerFeedback::MENU_SELECT:
-		ROS_INFO_STREAM("marker "<<feedback->marker_name <<" menu control was clicked");
-		marker_server_ptr_->applyChanges();
 		break;
 	}
 }
@@ -91,35 +119,127 @@ void InteractiveSurfaceServer::button_marker_callback(
 void InteractiveSurfaceServer::menu_marker_callback(
 		const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback)
 {
+
+	typedef std::map<std::string,bool>::iterator SelectionIterator;
+
 	switch(feedback->event_type)
 	{
 	case visualization_msgs::InteractiveMarkerFeedback::BUTTON_CLICK:
-		ROS_INFO_STREAM("marker "<<feedback->marker_name <<" button control was clicked");
+//		ROS_INFO_STREAM("marker "<<feedback->marker_name <<" button control was clicked");
+		toggle_selection_flag(feedback->marker_name);
 		marker_server_ptr_->applyChanges();
 		break;
 
 	case visualization_msgs::InteractiveMarkerFeedback::MENU_SELECT:
-		ROS_INFO_STREAM("marker "<<feedback->marker_name <<" menu control was clicked");
-		marker_server_ptr_->applyChanges();
+//		ROS_INFO_STREAM("marker: "<<feedback->marker_name <<", entry_id: "<< feedback->menu_entry_id
+//				<<", menu control was clicked");
+
+		if(feedback->menu_entry_id == select_entry_id_ )
+		{
+			set_selection_flag(feedback->marker_name,true);
+			marker_server_ptr_->applyChanges();
+			return;
+		}
+		else if(feedback->menu_entry_id == unselect_entry_id_)
+		{
+			set_selection_flag(feedback->marker_name,false);
+			marker_server_ptr_->applyChanges();
+			return;
+
+		}
+		else if(feedback->menu_entry_id == clear_all_entry_id_)
+		{
+
+			for(SelectionIterator i = surface_selection_map_.begin();i!= surface_selection_map_.end();i++)
+			{
+				set_selection_flag(i->first,false);
+			}
+
+			marker_server_ptr_->applyChanges();
+			return;
+
+		}
+		else if(feedback->menu_entry_id == select_all_entry_id_)
+		{
+			for(SelectionIterator i = surface_selection_map_.begin();i!= surface_selection_map_.end();i++)
+			{
+				set_selection_flag(i->first,true);
+			}
+
+			marker_server_ptr_->applyChanges();
+			return;
+
+		}
+		else if(feedback->menu_entry_id == hide_entry_id_)
+		{
+			ROS_WARN_STREAM("'Hide' menu option has not been implemented yet");
+
+			marker_server_ptr_->applyChanges();
+			return;
+		}
+		else if(feedback->menu_entry_id == show_all_entry_id_)
+		{
+			ROS_WARN_STREAM("'Show All' menu option has not been implemented yet");
+
+			marker_server_ptr_->applyChanges();
+			return;
+		}
+
 		break;
 	}
 }
 
-void InteractiveSurfaceServer::create_arrow_marker(const visualization_msgs::Marker& surface_marker,
-		visualization_msgs::Marker& arrow_marker)
+void InteractiveSurfaceServer::create_arrow_marker(
+		const visualization_msgs::Marker& surface_marker,visualization_msgs::Marker& arrow_marker)
 {
+	// create temporary point cloud
+	pcl::PointCloud<pcl::PointXYZ> surface;
+	surface.width = surface_marker.points.size();
+	surface.height = 1;
+	surface.points.resize(surface_marker.points.size());
+	for(int i = 0; i < surface_marker.points.size();i++)
+	{
+		surface.points[i].x = surface_marker.points[i].x;
+		surface.points[i].y = surface_marker.points[i].y;
+		surface.points[i].z = surface_marker.points[i].z;
+	}
+
+	// finding bouding box bounds
+	pcl::PointXYZ min,max;
+	pcl::getMinMax3D(surface,min,max);
+
+	// create arrow
+	arrow_marker.type = arrow_marker.ARROW;
+	arrow_marker.scale.x = arrow_shaft_diameter_;
+	arrow_marker.scale.y = arrow_head_diameter_;
+	arrow_marker.scale.z = arrow_head_length_;
+	arrow_marker.color.r = 0;
+	arrow_marker.color.g = arrow_marker.color.b = arrow_marker.color.a = 1;
+	arrow_marker.points.resize(2);
+
+	// start point
+	arrow_marker.points[1].x = 0.5f*(min.x + max.x);
+	arrow_marker.points[1].y = 0.5f*(min.y + max.y);
+	arrow_marker.points[1].z = max.z + arrow_distance_;
+
+	// end point
+	arrow_marker.points[0].x = arrow_marker.points[1].x;
+	arrow_marker.points[0].y = arrow_marker.points[1].y;
+	arrow_marker.points[0].z = arrow_marker.points[1].z + arrow_length_;
 
 }
 
-void InteractiveSurfaceServer::add_marker(const visualization_msgs::Marker& marker,
+void InteractiveSurfaceServer::add_surface(const visualization_msgs::Marker& marker,
 		const geometry_msgs::Pose &pose)
 {
 	// create marker
 	std::stringstream ss;
-	ss<<marker_name_<<"_" <<marker_names_.size() +1;
+	ss<<marker_name_<<"_" <<surface_selection_map_.size() +1;
 	visualization_msgs::InteractiveMarker int_marker;
 	int_marker.name = ss.str();
 	int_marker.pose = pose;
+
+
 
 	// create button control
 	visualization_msgs::InteractiveMarkerControl button_control;
@@ -128,10 +248,21 @@ void InteractiveSurfaceServer::add_marker(const visualization_msgs::Marker& mark
 	button_control.name = "button_" + int_marker.name;
 	button_control.always_visible = true;
 
+	// create seletected arrow marker
+	visualization_msgs::Marker arrow_marker;
+	create_arrow_marker(marker,arrow_marker);
+	arrow_marker.color.a = 0; // unselected
+	visualization_msgs::InteractiveMarkerControl selected_arrow;
+	selected_arrow.interaction_mode = selected_arrow.FIXED;
+	selected_arrow.markers.push_back(arrow_marker);
+	selected_arrow.name = "selected_"+int_marker.name;
+	selected_arrow.always_visible = true;
+
 	// fill interactive marker
 	int_marker.controls.push_back(button_control);
+	int_marker.controls.push_back(selected_arrow);
 	int_marker.scale = 1;
-	int_marker.header.frame_id = frame_id_;
+	int_marker.header.frame_id = marker.header.frame_id;
 	int_marker.description = marker_description_;
 
 	// add marker to server
@@ -139,17 +270,17 @@ void InteractiveSurfaceServer::add_marker(const visualization_msgs::Marker& mark
 	menu_handler_.apply(*marker_server_ptr_,int_marker.name);
 
 	// save name
-	marker_names_.push_back(int_marker.name);
+	surface_selection_map_.insert(std::make_pair(int_marker.name,false));
 
 	// apply changes
 	marker_server_ptr_->applyChanges();
 }
 
-void InteractiveSurfaceServer::add_marker(const visualization_msgs::Marker& marker)
+void InteractiveSurfaceServer::add_surface(const visualization_msgs::Marker& marker)
 {
 	geometry_msgs::Pose pose;
 	tf::poseTFToMsg(tf::Transform::getIdentity(),pose);
-	add_marker(marker,pose);
+	add_surface(marker,pose);
 }
 
 void InteractiveSurfaceServer::add_random_surface_marker()
@@ -170,7 +301,7 @@ void InteractiveSurfaceServer::add_random_surface_marker()
 	visualization_msgs::Marker marker;
 	create_polygon_marker(marker,rand() % defaults::MAX_TRIANGLES + 1);
 
-	add_marker(marker,pose);
+	add_surface(marker,pose);
 }
 
 
