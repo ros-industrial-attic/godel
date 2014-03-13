af106319a7c19cd3fc23aef845227e1bc91307d3
jrgnicho <jrgnicho@gmail.com>
2014-03-13 18:43:15 -0500

    added apache 2.0 license statement

--- godel_plugins/include/godel_plugins/rviz_panels/robot_blending_panel.h
+++ godel_plugins/include/godel_plugins/rviz_panels/robot_blending_panel.h
@@ -1,9 +1,18 @@
 /*
- * robot_blending_panel.h
- *
- *  Created on: Mar 13, 2014
- *      Author: ros developer 
- */
+	Copyright Mar 13, 2014 Southwest Research Institute
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
 
 #ifndef ROBOT_BLENDING_PANEL_H_
 #define ROBOT_BLENDING_PANEL_H_
