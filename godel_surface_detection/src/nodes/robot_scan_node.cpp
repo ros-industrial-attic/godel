4377042514a4b7a1b41503f249e0b8b8e678c7ea
jrgnicho <jrgnicho@gmail.com>
2014-04-15 11:27:03 -0500

    added robot scan node in order to test the scan trajectory generation and data collection

---
+++ godel_surface_detection/src/nodes/robot_scan_node.cpp
@@ -0,0 +1,57 @@
+/*
+	Copyright Apr 15, 2014 Southwest Research Institute
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
+#include <godel_surface_detection/scan/robot_scan.h>
+
+static const std::string DISPLAY_TRAJECTORY_TOPIC = "scan_trajectory";
+int main(int argc,char** argv)
+{
+	ros::init(argc,argv,"robot_scan_node");
+	ros::NodeHandle nh;
+
+	// publishers
+	ros::Publisher traj_pub = nh.advertise<moveit_msgs::DisplayTrajectory>(DISPLAY_TRAJECTORY_TOPIC,1,true);
+
+	godel_surface_detection::scan::RobotScan robot_scan;
+	if(robot_scan.load_parameters("~") && robot_scan.init())
+	{
+		moveit_msgs::DisplayTrajectory display_msg;
+		if(robot_scan.get_display_trajectory(display_msg))
+		{
+			ros::Duration loop_duration(0.5f);
+			while(ros::ok())
+			{
+				traj_pub.publish(display_msg);
+				loop_duration.sleep();
+			}
+		}
+		else
+		{
+			ROS_ERROR_STREAM("Failed to create scan trajectory");
+		}
+	}
+	else
+	{
+		ROS_ERROR_STREAM("Robot scan object did not initialized property");
+	}
+
+
+	return 0;
+}
+
+
+
+
