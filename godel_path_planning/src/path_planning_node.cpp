2d84153bb36cb515d4384957cf9a01059d460bad
Jonathan Meyer <jonathan.meyer@swri.org>
2015-02-12 10:00:50 -0600

    Adjusted godel_path_planning node with skeleton of Descartes planner

--- godel_path_planning/src/path_planning_node.cpp
+++ godel_path_planning/src/path_planning_node.cpp
@@ -24,61 +24,23 @@
 
 
 #include <ros/ros.h>
-#include <boost/bind.hpp>
-#include <boost/foreach.hpp>
-#include "godel_msgs/BlendingPlan.h"
-#include "godel_msgs/ProcessPlanning.h"
-#include "godel_process_path_generation/process_path_generator.h"
+#include "godel_path_planning/descartes_bridge.h"
+#include "godel_msgs/TrajectoryPlanning.h"
 
-
-bool path_planner_cb(godel_msgs::BlendingPlanRequest &req, godel_msgs::BlendingPlanResponse &res,
-                     godel_process_path::ProcessPathGenerator *ppg,
-                     ros::ServiceClientPtr process_planner_client, ros::ServiceClientPtr move_group_client)
+bool trajectory_planning_callback(godel_msgs::TrajectoryPlanning::Request& req,
+                                  godel_msgs::TrajectoryPlanning::Response& res)
 {
-  ROS_WARN("Path planner callback not implemented yet!");
-  return false;
-
-  std::vector<trajectory_msgs::JointTrajectory> trajectories;
-  BOOST_FOREACH(const godel_msgs::SurfaceBoundaries &surface, req.surfaces)
-  {
-    // Call process planner to get process plan
-    godel_msgs::ProcessPlanning process_planner_srv;
-    process_planner_srv.request.params = req.params;
-    process_planner_srv.request.surface.boundaries = surface.boundaries;
-    process_planner_client->call(process_planner_srv);
-
-    // Call trajectory planner to get trajectory
-    //TODO trajectories.push_back(trajectory_planning.result.trajectory);
-  }
-  res.trajectory.points.clear();
-  //TODO fill out joint trajectory names, header, etc.
-  for (std::vector<trajectory_msgs::JointTrajectory>::iterator trajectory=boost::next(trajectories.begin());
-       trajectory!=trajectories.end(); ++trajectory)
-  {
-    //stitcher_srv
-    //sticher_srv.request.from_pt = boost::prior(trajectory)->back();
-    //sticher_srv.request.to_pt = trajectory->front();
-    //move_group_client->call(stitcher_srv);
-    //res.trajectory.points.insert(res.trajectory.points.end(), stitcher_srv.res.trajectory.begin(), stitcher_srv.res.trajectory.end());
-  }
   return true;
 }
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "godel_path_planner");
+  ros::init(argc, argv, "godel_trajectory_planner");
   ros::NodeHandle nh;
 
-  godel_process_path::ProcessPathGenerator ppg;
-  ros::ServiceClientPtr process_path_generation_client(new ros::ServiceClient(nh.serviceClient<godel_msgs::ProcessPlanning>("process_path_generator")));
-  ros::ServiceClientPtr move_group_client; // TODO add client for move_group for "stitching" trajectories together
+  ros::ServiceServer trajectory_server = nh.advertiseService("trajectory_planner", trajectory_planning_callback);
 
-  ros::ServiceServer path_planner_service = nh.advertiseService<godel_msgs::BlendingPlanRequest,
-                                                                godel_msgs::BlendingPlanResponse>
-                                            ("path_planner",
-                                             boost::bind(path_planner_cb, _1, _2, &ppg, process_path_generation_client, move_group_client));
-
-  ROS_INFO("%s ready to service requests.", path_planner_service.getService().c_str());
+  ROS_INFO("%s ready to service requests.", trajectory_server.getService().c_str());
 
   ros::spin();
 
