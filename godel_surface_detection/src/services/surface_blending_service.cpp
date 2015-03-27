5e4b6f5c4b4519ab65b7c24cf126aa59e2aed55a
Jonathan Meyer <jonathan.meyer@swri.org>
2015-03-27 16:59:10 -0500

    Small modifications to the surface blending service to move literals to constants

--- godel_surface_detection/src/services/surface_blending_service.cpp
+++ godel_surface_detection/src/services/surface_blending_service.cpp
@@ -60,14 +60,23 @@ const float TOOL_SHAFT_DIA = .006;
 const float TOOL_SHAFT_LEN = .045;
 const std::string TOOL_FRAME_ID = "process_tool";
 
-// Temporary repository for constants that will be replaced
-const static std::string TRAJECTORY_BAGFILE = "trajectory.bag";
-const static std::string TRAJECTORY_GROUP_NAME = "manipulator_tcp";
-const static std::string TRAJECTORY_TOOL_FRAME = "tcp_frame";
-const static std::string TRAJECTORY_WORLD_FRAME = "world_frame";
-const static uint8_t TRAJECTORY_ITERATIONS = 2;
-const static double TRAJECTORY_ANGLE_DISC = 0.3;
-const static double TRAJECTORY_INTERPOINT_DELAY = 0.5;
+// Temporary constants for storing blending path planning parameters
+// Will be replaced by loadable, savable parameters
+const static std::string BLEND_TRAJECTORY_BAGFILE = "blend_trajectory.bag";
+const static std::string BLEND_TRAJECTORY_GROUP_NAME = "manipulator_tcp";
+const static std::string BLEND_TRAJECTORY_TOOL_FRAME = "tcp_frame";
+const static std::string BLEND_TRAJECTORY_WORLD_FRAME = "world_frame";
+const static double BLEND_TRAJECTORY_ANGLE_DISC = M_PI/10.0;
+const static double BLEND_TRAJECTORY_INTERPOINT_DELAY = 0.5;
+
+// Temporary constants for storing scan path planning parameters
+// Will be replaced by loadable, savable parameters
+const static std::string SCAN_TRAJECTORY_BAGFILE = "scan_trajectory.bag";
+const static std::string SCAN_TRAJECTORY_GROUP_NAME = "manipulator_keyence";
+const static std::string SCAN_TRAJECTORY_TOOL_FRAME = "keyence_tcp_frame";
+const static std::string SCAN_TRAJECTORY_WORLD_FRAME = "world_frame";
+const static double SCAN_TRAJECTORY_ANGLE_DISC = 0.2;
+const static double SCAN_TRAJECTORY_INTERPOINT_DELAY = 0.5;
 
 struct ProcessPathDetails
 {
@@ -387,7 +396,7 @@ protected:
 				BOOST_FOREACH(godel_process_path::PolygonBoundary &bnd, filtered_boundaries)
 				{
 					godel_surface_detection::ProfilimeterScanParams param;
-					param.width_ = 0.04;
+					param.width_ = 0.02;
 					param.overlap_ = 0.0;
 
 					// Filter and reverse boundaries
@@ -422,9 +431,7 @@ protected:
 				}
 				tool_path_markers_pub_.publish(boundary_markers);       // Pre-publish boundaries before completing offset
 
-				static int scan_marker_id = 0;
-				// TODO DEBUG
-				// 				for(int j =0; j < boundary_markers.markers.size();j++)
+				// Publish the raster paths
 				for(int j =0; j < scan_markers.markers.size();j++)
 				{
 					visualization_msgs::Marker &m = scan_markers.markers[j];
@@ -446,7 +453,6 @@ protected:
 				// calling visualization service and saving results
 				if(visualize_process_path_client_.call(process_plan))
 				{
-
 					// create path marker
 					path_marker = process_plan.response.path;
 					path_marker.header.frame_id = mesh.header.frame_id;
@@ -573,12 +579,11 @@ protected:
 		{
 			godel_msgs::TrajectoryPlanning plan;
 			// Set planning parameters		
-			plan.request.group_name = "manipulator_keyence";
-			plan.request.tool_frame = "keyence_tcp_frame";
-			plan.request.world_frame = TRAJECTORY_WORLD_FRAME;
-			plan.request.iterations = TRAJECTORY_ITERATIONS;
-			plan.request.angle_discretization = 0.2;
-			plan.request.interpoint_delay = TRAJECTORY_INTERPOINT_DELAY;
+			plan.request.group_name = SCAN_TRAJECTORY_GROUP_NAME;
+			plan.request.tool_frame = SCAN_TRAJECTORY_TOOL_FRAME;
+			plan.request.world_frame = SCAN_TRAJECTORY_WORLD_FRAME;
+			plan.request.angle_discretization = SCAN_TRAJECTORY_ANGLE_DISC;
+			plan.request.interpoint_delay = SCAN_TRAJECTORY_INTERPOINT_DELAY;
 			plan.request.is_blending_path = false;
 			
 			const visualization_msgs::Marker& marker = process_path_results_.scan_paths_.markers[i];
@@ -604,9 +609,9 @@ protected:
 		// save to file for easier testing
 		if (trajectories.size() > 0)
 		{
-			ROS_INFO_STREAM("Saving trajectories to bagfile: " << "scan_plan.bag");
+			ROS_INFO_STREAM("Saving trajectories to bagfile: " << SCAN_TRAJECTORY_BAGFILE);
 			rosbag::Bag bag;
-			bag.open("scan_plan.bag", rosbag::bagmode::Write);
+			bag.open(SCAN_TRAJECTORY_BAGFILE, rosbag::bagmode::Write);
 			for (std::size_t i = 0; i < trajectories.size(); ++i)
 			{
 				bag.write("trajectory", ros::Time::now(), trajectories[i]);
@@ -625,12 +630,11 @@ protected:
 		{
 			godel_msgs::TrajectoryPlanning plan;
 			// Set planning parameters		
-			plan.request.group_name = TRAJECTORY_GROUP_NAME;
-			plan.request.tool_frame = TRAJECTORY_TOOL_FRAME;
-			plan.request.world_frame = TRAJECTORY_WORLD_FRAME;
-			plan.request.iterations = TRAJECTORY_ITERATIONS;
-			plan.request.angle_discretization = TRAJECTORY_ANGLE_DISC;
-			plan.request.interpoint_delay = TRAJECTORY_INTERPOINT_DELAY;
+			plan.request.group_name = BLEND_TRAJECTORY_GROUP_NAME;
+			plan.request.tool_frame = BLEND_TRAJECTORY_TOOL_FRAME;
+			plan.request.world_frame = BLEND_TRAJECTORY_WORLD_FRAME;
+			plan.request.angle_discretization = BLEND_TRAJECTORY_ANGLE_DISC;
+			plan.request.interpoint_delay = BLEND_TRAJECTORY_INTERPOINT_DELAY;
 			plan.request.is_blending_path = true;
 
 			const visualization_msgs::Marker& marker = process_path_results_.process_paths_.markers[i];
@@ -656,16 +660,16 @@ protected:
 		// save to file for easier testing
 		if (trajectories.size() > 0)
 		{
-			ROS_INFO_STREAM("Saving trajectories to bagfile: " << TRAJECTORY_BAGFILE);
+			ROS_INFO_STREAM("Saving trajectories to bagfile: " << BLEND_TRAJECTORY_BAGFILE);
 			rosbag::Bag bag;
-			bag.open(TRAJECTORY_BAGFILE, rosbag::bagmode::Write);
+			bag.open(BLEND_TRAJECTORY_BAGFILE, rosbag::bagmode::Write);
 			for (std::size_t i = 0; i < trajectories.size(); ++i)
 			{
 				bag.write("trajectory", ros::Time::now(), trajectories[i]);
 			}
 		}
 
-		// ROS_WARN_STREAM("Setting scan callback!");
+		// After the blend path completes, signal the profilimeter planning function to run
 		scan_planning_timer_.setPeriod(ros::Duration(0.1f));
 		scan_planning_timer_.start();
 	}
