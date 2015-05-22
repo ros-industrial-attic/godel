9bd0820d18bc2f5689085b8aed0d8eb0d1202700
Jonathan Meyer <jonathan.meyer@swri.org>
2015-05-22 14:26:39 -0500

    Added a new moveit configuration for the irb2400

---
+++ abb_irb2400_descartes/src/plugin_init.cpp
@@ -0,0 +1,4 @@
+#include <abb_irb2400_descartes/abb_irb2400_robot_model.h>
+#include <pluginlib/class_list_macros.h>
+
+PLUGINLIB_EXPORT_CLASS(abb_irb2400_descartes::AbbIrb2400RobotModel,descartes_core::RobotModel)
