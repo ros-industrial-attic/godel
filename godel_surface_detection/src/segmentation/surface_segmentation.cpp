be639bc9e11d56fee860d1bc3549e5e366b28e11
Jonathan Meyer <Jmeyer@swri.org>
2017-03-24 09:57:33 -0500

    Adjusted parameters to smooth surface and stop robot flipping

--- godel_surface_detection/src/segmentation/surface_segmentation.cpp
+++ godel_surface_detection/src/segmentation/surface_segmentation.cpp
@@ -419,7 +419,8 @@ void SurfaceSegmentation::computeNormals()
   ne.setInputCloud (input_cloud_);
   pcl::search::KdTree<pcl::PointXYZRGB>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZRGB> ());
   ne.setSearchMethod (tree);
-  ne.setKSearch (100);
+  ne.setRadiusSearch(0.025);
+//  ne.setKSearch (100);
 
   // Estimate the normals
   ne.compute (*normals_);
