9ed032a721ea610064d4e2de3c2546fe4f6eb1c7
Jonathan Meyer <jmeyer@swri.org>
2017-05-17 16:35:08 -0500

    Added some basic code for breaking out the collection of point cloud data from the fusion of it and furthermore from the 'scoring' of it.

---
+++ godel_scan_analysis/include/godel_scan_analysis/profile_scan_fusion.h
@@ -0,0 +1,30 @@
+#ifndef GODEL_PROFILE_SCAN_FUSION_H
+#define GODEL_PROFILE_SCAN_FUSION_H
+
+#include <pcl_ros/point_cloud.h>
+#include <tf/tf.h>
+
+namespace godel_scan_analysis
+{
+
+struct ProfileFusionParameters
+{
+  double voxel_leaf_size;
+};
+
+/**
+ * @brief The 'fuseProfiles' function merges a collection of profiles taken at very high resolution and frequency
+ * at the locations given by 'poses'. This process involves filtering the data prior to fusion, and downsampling
+ * the data after combining.
+ * @param profiles
+ * @param poses
+ * @param params
+ * @return
+ */
+pcl::PointCloud<pcl::PointXYZ>::Ptr
+fuseProfiles(const std::vector<pcl::PointCloud<pcl::PointXYZ>>& profiles, const std::vector<tf::Transform>& poses,
+             const ProfileFusionParameters& params);
+
+}
+
+#endif // GODEL_PROFILE_SCAN_FUSION_H
